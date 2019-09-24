9106aff905231887c634c2c798e19ab704387eb7
this-is-batman <abhirupgupta234@rediffmail.com>
2019-09-25 01:36:59 +0530

    Find PI to the Nth digit

---
+++ pi_n.py
@@ -0,0 +1,14 @@
+# get the number of digits after decimal in pi
+import math
+
+def get_digit_after_pi():
+    n = input("Enter the number of digits after decimal of pi that you want to keep?\n")
+    if int(n)>20:
+        print("The hard limit is 20 digits after decimal in pi.")
+        exit(-1)
+    str = '{:0.'+ n + '' +'f}'
+    ans = str.format(math.pi)
+    print(ans)
+
+if __name__ == "__main__":
+     get_digit_after_pi()
