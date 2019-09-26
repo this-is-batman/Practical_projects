cd3f469ed1521db684b0f2b2d42e0472fdd4f38e
this-is-batman <abhirupgupta234@rediffmail.com>
2019-09-26 22:23:46 +0530

    Minor corrections

--- Numbers/fib_seq.py
+++ Numbers/fib_seq.py
@@ -1,8 +1,8 @@
 # generate the fibonacci sequence upto the Nth number taken from input
 fib=[] #empty list
 def find_fib(n):
-    fib.append(0);
-    fib.append(1);
+    fib.append(0)
+    fib.append(1)
     for i in range(2,n):
         fib.append(fib[i-1]+fib[i-2])
     return fib
